467a2afdf23cdfd5e75e2c9ffdaecd64b0e90771
kangdonguk <kangdongouk@gmail.com>
2022-01-07 17:32:00 +0900

    #4.3 Singleton Pattern

--- main.go
+++ main.go
@@ -1,50 +1,12 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
+	"log"
+
+	"github.com/kadragon/nomadcoin/blockchain"
 )
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	// 시작 block이 아닐 경우 prevHash 가져오기
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockchain) listBlocks() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("Prev: %s\n", block.prevHash)
-	}
-}
-
 func main() {
-	chain := blockchain{}
-
-	chain.addBlock("Genesis Block")
-	chain.addBlock("Second Block")
-
-	chain.listBlocks()
+	chain := blockchain.GetBlockchain()
+	log.Printf("%#v", chain)
 }
