12d3f5c9873adcb13c63955e2b23f15acba32905
kangdonguk <kangdongouk@gmail.com>
2022-01-07 12:22:30 +0900

    #3.2 Functions

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import "fmt"
 
-const outname string = "nico"
+func plus(a, b int, name string) (int, string) {
+	return a + b, name
+}
+
+func multiplus(a ...int) int {
+	var total int = 0
+	for _, val := range a {
+		total += val
+	}
+
+	return total
+}
 
 func main() {
-	var name string = "nico"
-	name = "12"
+	result, name := plus(2, 2, "nico")
+	fmt.Println(result, name)
 
-	// in function only
-	newName := "nico"
+	result = multiplus(2, 2, 4, 5, 1, 2, 3)
+	fmt.Println(result)
 
-	fmt.Println(name, newName, outname)
+	var names string = "Nicolas!! is my name"
+	for idx, letter := range names {
+		fmt.Println(idx, letter, string(letter))
+	}
 }
