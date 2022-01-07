3a2403c986a3e435fdd2c715f7ba828257986cb2
kangdonguk <kangdongouk@gmail.com>
2022-01-07 12:13:13 +0900

    #3.1 Variables in Go

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+const outname string = "nico"
+
 func main() {
-	fmt.Println("It works!")
+	var name string = "nico"
+	name = "12"
+
+	// in function only
+	newName := "nico"
+
+	fmt.Println(name, newName, outname)
 }
