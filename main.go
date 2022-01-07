67cd843b6f5370bc35b5a29307018a35add151ee
kangdonguk <kangdongouk@gmail.com>
2022-01-07 16:49:42 +0900

    #3.6 Structs

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) seyHello() {
+	fmt.Printf("Hello! My name is %s and I'm %d\n", p.name, p.age)
+}
+
+func (p person) seyKoreaAge() {
+	fmt.Printf("My Korea Age is %d\n", p.age+1)
+}
+
 func main() {
-	var a int = 2
-	var b int = a
-	var c *int = &a
-
-	a = 12
-
-	fmt.Println(b)
-	fmt.Println(*c)
+	nico := person{name: "nico", age: 12}
+	nico.seyHello()
+	nico.seyKoreaAge()
 }
