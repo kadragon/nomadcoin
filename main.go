0e6e77f9ca55dce952153a54c53b1e1e7e22b25f
kangdonguk <kangdongouk@gmail.com>
2022-01-07 17:01:53 +0900

    #4.0 Introduction

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/kadragon/nomadcoin/person"
-)
-
 func main() {
-	nico := person.Person{}
 
-	nico.SetDetails("nico", 12)
-	log.Println(nico)
-
-	nico.SetDetailsWithPointer("nico", 12)
-	log.Println(nico)
 }
