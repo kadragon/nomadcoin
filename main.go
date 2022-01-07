3db2f8841b98e10f3292a52ef34de28c70cbd8f7
kangdonguk <kangdongouk@gmail.com>
2022-01-07 17:25:55 +0900

    #4.2 Our First Blockchain

--- main.go
+++ main.go
@@ -11,10 +11,40 @@ type block struct {
 	prevHash string
 }
 
-func main() {
-	genesisBlock := block{"Genesis Block", "", ""}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
-	genesisBlock.hash = fmt.Sprintf("%x\n", hash)
+type blockchain struct {
+	blocks []block
+}
 
-	fmt.Println(genesisBlock)
+func (b *blockchain) getLastHash() string {
+	// 시작 block이 아닐 경우 prevHash 가져오기
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev: %s\n", block.prevHash)
+	}
+}
+
+func main() {
+	chain := blockchain{}
+
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+
+	chain.listBlocks()
 }
