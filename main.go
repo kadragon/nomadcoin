1463b247c8f8d9b5429b012547dbd47854a06897
kangdonguk <kangdongouk@gmail.com>
2022-01-13 16:12:34 +0900

    #5.5 Refactoring

--- main.go
+++ main.go
@@ -1,54 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/kadragon/nomadcoin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := homeData{"Home", blockchain.GetBlockchain().Allblocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		if err := r.ParseForm(); err != nil {
-			log.Fatal(err)
-		}
-
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+import "github.com/kadragon/nomadcoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
