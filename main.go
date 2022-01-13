513686cf1803fbfec1c0d85ee67d40999b4d955d
kangdonguk <kangdongouk@gmail.com>
2022-01-13 16:32:51 +0900

    #6.0 Setup

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/kadragon/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/kadragon/nomadcoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s\n", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
